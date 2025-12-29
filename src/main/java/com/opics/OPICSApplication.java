4f6b0b825e799e09cd42f22b62f13ec2e3424cf3
sohailcodes23 <sohail2769@gmail.com>
2025-12-29 22:31:22 +0530

    Initial setup

---
+++ src/main/java/com/opics/OPICSApplication.java
@@ -0,0 +1,13 @@
+package com.opics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OPICSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OPICSApplication.class, args);
+	}
+
+}
